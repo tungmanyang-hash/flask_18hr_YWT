6d062513b2f4ff1ab68626359dba62c4429507a1
Chrisy <tungmanyang@gmail.com>
2026-09-03 19:06:23 +0800

    0903_ispan

--- ch06/extends(practice).py
+++ ch06/extends(practice).py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 
 #----------practice start------------
-
+@app.route('/')
+def index():
+    return render_template("basic_extends(practice).html", page_header = "Page header")
 #----------practice end-------------- 
 
 
