e5e6f1e1a3acff85fbe0b0e8f06c459e7a218e5f
Chrisy <tungmanyang@gmail.com>
2026-09-03 19:38:56 +0800

    0903_ispan

--- ch06/extends(practice).py
+++ ch06/extends(practice).py
@@ -9,7 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("basic_extends(practice).html", page_header = "Page header")
+
+@app.route('/block')
+def block():
+    return render_template("block(practice).html")
+
+@app.route('/super')
+def jinja2_super():
+    return render_template("super(practice).html", page_header = "test super()")
+
 #----------practice end-------------- 
+#extend 可以限定繼承，include 只能原內容照搬，不能客製
 
 
 if __name__=="__main__":
