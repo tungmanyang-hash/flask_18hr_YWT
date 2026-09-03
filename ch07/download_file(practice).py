6a72dced64e2ccd209826b1cb7388f0b214781ba
Chrisy <tungmanyang@gmail.com>
2026-09-03 20:35:25 +0800

    0903_ispan

--- ch07/download_file(practice).py
+++ ch07/download_file(practice).py
@@ -12,6 +12,15 @@ def index():
 
 
 # ----------practice start------------
+@app.route('/download_file', methods=['GET', 'POST'])
+def download_file():
+    if request.method == "GET":
+        return render_template('download(practice).html', page_header="download file")
+    elif request.method == "POST":
+        file_name = request.form['download_file']
+        file_dir = Path(__file__).resolve().parent/'uploaded'
+    return send_from_directory(file_dir, file_name, as_attachment=False)  # as_attachment=False : render file by browser
+
 # ----------practice end-----------
 
 if __name__ == "__main__":
--- ch07/file(practice).py
+++ ch07/file(practice).py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 
 # practice start
+from pathlib import Path
+import uuid
 # practice end
 
 app = Flask(__name__)
 
 # practice start
+UPLOAD_FOLDER = Path(__file__).resolve().parent/'uploaded'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
 # practice end
 
 @app.route('/')
@@ -15,6 +20,20 @@ def index():
 
 
 # practice start
+@app.route('/file', methods=['GET', 'POST'])
+def get_file():
+    if request.method == "GET":
+        return render_template('file(practice).html', page_header="upload file")
+    elif request.method == "POST":
+        file = request.files['file'] # key is the value of name(html attribute) in <input type="file">
+        if file:
+            filename = str(uuid.uuid4())+"_"+file.filename
+            file.save(UPLOAD_FOLDER/filename)
+        data = [["method:", request.method],
+                ["base_url:", request.base_url],
+                ["file:",request.files]]
+        return render_template('form_result(practice).html', page_header="review file", data=data)
+
 # practice end
 
 
