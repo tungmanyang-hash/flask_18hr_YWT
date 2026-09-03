e2ec50e5a20e4858c831b146f32403c01d698f79
Chrisy <tungmanyang@gmail.com>
2026-09-03 18:37:31 +0800

    0903_ispan

--- ch06/include(practice).py
+++ ch06/include(practice).py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 
 #----------practice start------------
+@app.route('/')
+def index():
+    return render_template("include(practice).html", page_header="include")
 #----------practice end-------------- 
 
 
 if __name__=="__main__":
     app.run(debug=True)
+ 
